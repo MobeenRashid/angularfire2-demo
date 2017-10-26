b1d1f879664eace09f8232f6832490f4af35ec10
Mobeen Rashid <mobeenrashed032@gmail.com>
2017-10-26 11:50:18 +0500

    tidy up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { Subscription } from 'rxjs/Subscription';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnDestroy {
+
+  courses;
+  course$;
+  author$;
+  subscription: Subscription;
+  fireList: AngularFireList<{}>;
+
+  constructor(private db: AngularFireDatabase) {
+    this.fireList = this.db.list('/courses');
+    this.courses = this.fireList.valueChanges();
+
+    // this.subscription = this.fireList.valueChanges().subscribe(response => {
+    //   this.courses = response;
+    //   console.log(response);
+    // });
+
+    this.course$ = this.db.object('/courses/1').valueChanges();
+    this.author$ = this.db.object('/author').valueChanges();
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  add(course: HTMLInputElement) {
+    this.fireList.push({
+      name: course.value,
+      price: 123,
+      sections: [
+        { name: 'Directives' },
+        { name: 'Pipes' }
+      ]
+    });
+    course.value = '';
+  }
+
+  update(course) {
+    this.fireList.update('5', { name: 'new name' });
+    console.log(course.$key);
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyA4dDba7T-2LcRRHc1vPZcO9Bbu457v8EA',
+    authDomain: 'fir-demo-7e206.firebaseapp.com',
+    databaseURL: 'https://fir-demo-7e206.firebaseio.com',
+    projectId: 'fir-demo-7e206',
+    storageBucket: 'fir-demo-7e206.appspot.com',
+    messagingSenderId: '862773025458'
+  }
 };
